6677944a981bb76b7508ab53899b0cb052c380a8
dorinpaval <paval.stelica@yahoo.com>
2022-03-07 20:42:49 -0600

    paval

---
+++ w1d4lecture2.js
@@ -0,0 +1,20 @@
+"use strict";
+
+let exp = ["{", "{", "}", "}"];
+let balanced = [];
+let okk = 1;
+for (let sign of exp) {
+    if (sign === "{"){
+        balanced.push("{");
+    }
+    else if (sign === "}" && balanced.length > 0) {
+        balanced.pop();
+    }
+        else
+            okk=0;
+}
+
+if (okk === 1 && balanced.length===0)
+    console.log("Balanced");
+else
+    console.log("Not Balanced");
