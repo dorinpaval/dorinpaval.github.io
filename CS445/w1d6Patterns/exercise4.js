8f02e028f5c4547c8c6ee4c1af6377a8aaa627c6
dorinpaval <paval.stelica@yahoo.com>
2022-05-04 15:16:31 -0500

    paval

--- CS445/w1d6Patterns/exercise4.js
+++ CS445/w1d6Patterns/exercise4.js
@@ -1,19 +1,22 @@
 "use strict";
-let obj={};
-function fibonacci(n) {
+const fibonacci= (function(){
+    let obj={};
+    
+    function f(n) {
     let value;
     if (n in obj) {
         value = obj[n];
       } else {
         if (n === 0 || n === 1)
           value = 1;
-        else
-          value = fibonacci(n - 1,obj) + fibonacci(n - 2,obj);
-          obj[n] = value;
+        else{
+          value = f(n - 1,obj) + f(n - 2,obj);
+          obj[n] = value;}
       }
-  
       return value;
-}
+    }
+    return f;
+})();
 
-let dada=fibonacci(10);
+let dada=fibonacci(5);
 console.log(dada);
